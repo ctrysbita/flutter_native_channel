26299bee699ca95acded5909de43c531e6574e3d
Jason C.H <ctrysbita@outlook.com>
2020-12-17 19:56:57 +0800

    Update example to split test cases

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -6,7 +6,8 @@ import 'package:flutter_native_channel/flutter_native_channel.dart';
 const channel = MethodChannel('flutter_native_channel_example');
 final synchronousChannel =
     SynchronousMethodChannel('flutter_native_channel_example');
-final nativeChannel = ConcurrentMethodChannel('flutter_native_channel_example');
+final concurrentChannel =
+    ConcurrentMethodChannel('flutter_native_channel_example');
 
 void main() {
   runApp(MyApp());
@@ -25,65 +26,8 @@ class _MyAppState extends State<MyApp> {
   int channelTs = 0;
   int syncBinTs = 0;
   int syncChannelTs = 0;
-  int asyncBinTs = 0;
-  int asyncChannelTs = 0;
-
-  void handleTest() async {
-    var ts = DateTime.now();
-    await Future.wait<dynamic>([
-      for (var i = 0; i < count; i++)
-        ServicesBinding.instance.defaultBinaryMessenger
-            .send('flutter_bin_channel', ByteData(5)),
-    ]);
-    binTs = DateTime.now().difference(ts).inMicroseconds;
-
-    setState(() {});
-
-    ts = DateTime.now();
-    await Future.wait<dynamic>([
-      for (var i = 0; i < count; i++)
-        channel.invokeMethod<dynamic>('MTD', null),
-    ]);
-    channelTs = DateTime.now().difference(ts).inMicroseconds;
-
-    setState(() {});
-
-    ts = DateTime.now();
-    await Future.wait<dynamic>([
-      for (var i = 0; i < count; i++)
-        ConcurrentNativeBinaryMessenger.instance.send(1234, null),
-    ]);
-    asyncBinTs = DateTime.now().difference(ts).inMicroseconds;
-
-    setState(() {});
-
-    ts = DateTime.now();
-    await Future.wait<dynamic>([
-      for (var i = 0; i < count; i++)
-        nativeChannel.invokeMethod<dynamic>('MTD', null),
-    ]);
-    asyncChannelTs = DateTime.now().difference(ts).inMicroseconds;
-
-    setState(() {});
-
-    ts = DateTime.now();
-    for (var i = 0; i < count; i++) {
-      await SynchronousNativeBinaryMessenger().send(1234, null);
-    }
-    syncBinTs = DateTime.now().difference(ts).inMicroseconds;
-
-    setState(() {});
-
-    ts = DateTime.now();
-    for (var i = 0; i < count; i++) {
-      synchronousChannel.invokeMethod<dynamic>('MTD', null);
-    }
-    syncChannelTs = DateTime.now().difference(ts).inMicroseconds;
-
-    await Future<Null>.delayed(Duration(seconds: 1));
-
-    setState(() {});
-  }
+  int concurrentBinTs = 0;
+  int concurrentChannelTs = 0;
 
   @override
   Widget build(BuildContext context) {
@@ -113,21 +57,84 @@ class _MyAppState extends State<MyApp> {
               max: 30,
             ),
             Text('$count * $sizeInKb KB'),
-            Text('via flutter bin: $binTs us'),
-            Text('via flutter channel: $channelTs us'),
-            Text('via sync native bin: $syncBinTs us'),
-            Text('via sync native channel: $syncChannelTs us'),
-            Text('via async native bin: $asyncBinTs us'),
-            Text('via async native channel: $asyncChannelTs us'),
+            RaisedButton(
+              child: Text('via flutter async bin: $binTs us'),
+              onPressed: () async {
+                var ts = DateTime.now();
+                await Future.wait<dynamic>([
+                  for (var i = 0; i < count; i++)
+                    ServicesBinding.instance.defaultBinaryMessenger
+                        .send('flutter_bin_channel', ByteData(5)),
+                ]);
+                binTs = DateTime.now().difference(ts).inMicroseconds;
+                setState(() {});
+              },
+            ),
+            RaisedButton(
+              child: Text('via flutter async channel: $channelTs us'),
+              onPressed: () async {
+                var ts = DateTime.now();
+                await Future.wait<dynamic>([
+                  for (var i = 0; i < count; i++)
+                    channel.invokeMethod<dynamic>('MTD', null),
+                ]);
+                channelTs = DateTime.now().difference(ts).inMicroseconds;
+                setState(() {});
+              },
+            ),
+            RaisedButton(
+              child: Text('via concurrent native bin: $concurrentBinTs us'),
+              onPressed: () async {
+                var ts = DateTime.now();
+                await Future.wait<dynamic>([
+                  for (var i = 0; i < count; i++)
+                    ConcurrentNativeBinaryMessenger.instance.send(1234, null),
+                ]);
+                concurrentBinTs = DateTime.now().difference(ts).inMicroseconds;
+                setState(() {});
+              },
+            ),
+            RaisedButton(
+              child: Text(
+                  'via concurrent native channel: $concurrentChannelTs us'),
+              onPressed: () async {
+                var ts = DateTime.now();
+                await Future.wait<dynamic>([
+                  for (var i = 0; i < count; i++)
+                    concurrentChannel.invokeMethod<dynamic>('MTD', null),
+                ]);
+                concurrentChannelTs =
+                    DateTime.now().difference(ts).inMicroseconds;
+                setState(() {});
+              },
+            ),
+            RaisedButton(
+              child: Text('via sync native bin: $syncBinTs us'),
+              onPressed: () async {
+                var ts = DateTime.now();
+                for (var i = 0; i < count; i++) {
+                  SynchronousNativeBinaryMessenger().send(1234, null);
+                }
+                syncBinTs = DateTime.now().difference(ts).inMicroseconds;
+                setState(() {});
+              },
+            ),
+            RaisedButton(
+              child: Text('via sync native channel: $syncChannelTs us'),
+              onPressed: () async {
+                var ts = DateTime.now();
+                for (var i = 0; i < count; i++) {
+                  synchronousChannel.invokeMethod<dynamic>('MTD', null);
+                }
+                syncChannelTs = DateTime.now().difference(ts).inMicroseconds;
+                setState(() {});
+              },
+            ),
             RaisedButton(
               child: Text('Force GC'),
               onPressed: () => channel.invokeMethod<Null>('g'),
             ),
           ],
-        ),
-        floatingActionButton: FloatingActionButton(
-          child: Icon(Icons.play_arrow),
-          onPressed: handleTest,
         ),
       ),
     );
